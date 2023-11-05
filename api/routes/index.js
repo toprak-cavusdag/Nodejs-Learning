86ad18d94164abf88fbb8ec905b925dd7a131f19
toprak-cavusdag <cavusdagtoprak@gmail.com>
2023-11-05 21:56:09 +0300

    last

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
+const config = require('../config');
 var router = express.Router();
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Toprak',  config  });
 });
 
 module.exports = router;
